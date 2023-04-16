5b7aa07bb36ee974bd6d1c597ea0e56b59424dcf
iTBoyer <iTBoyer@qq.com>
2023-04-16 18:58:54 +0800

    添加中文支持，使用ai翻译openAI相关Localizable

--- ChatGPT/ContentView.swift
+++ ChatGPT/ContentView.swift
@@ -22,3 +22,13 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+/**
+This Swift code defines a user interface for a small app called "ChatGPT" using the SwiftUI framework. The user interface consists of a single text view that displays the text "ChatGPT & Xcode" in a large, condensed font. The text view is centered on the screen and has padding to provide some space around the text.
+
+The code also provides a preview of the user interface with the `ContentView_Previews` struct, which can be used to visualize the user interface in Xcode's interface builder.
+*/
+/**
+这段Swift代码定义了一个使用SwiftUI框架的小型应用程序"C hatGPT"的用户界面。用户界面由一个单独的文本视图组成，以大号、压缩字体显示文本"ChatGPT＆Xcode"。文本视图居中于屏幕并具有填充以在文本周围提供一些空间。
+
+代码还提供了一个用户界面的预览，使用`ContentView_Previews`结构体，在Xcode的界面构建器中可以使用它来可视化用户界面。
+*/
